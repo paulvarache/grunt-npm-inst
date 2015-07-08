3d945a0a588bfe8a39e24601e068153a94e8834e
Paul Varache <perso@paulvarache.ninja>
2015-07-08 14:37:28 +0200

    Added support for --prodution flag

--- tasks/index.js
+++ tasks/index.js
@@ -3,7 +3,9 @@ var spawn = require('child_process').spawn;
 module.exports = function (grunt) {
     grunt.registerMultiTask('npm_install', '', function () {
         var done = this.async();
-        var npm = spawn('npm', ['install'], { cwd: this.data.cwd || process.cwd() });
+        var params = ['install'];
+        if (this.data.production) params.push('--production');
+        var npm = spawn('npm', params, { cwd: this.data.cwd || process.cwd() });
         npm.stdout.pipe(process.stdout);
         npm.stderr.pipe(process.stderr);
         npm.on('exit', done);
